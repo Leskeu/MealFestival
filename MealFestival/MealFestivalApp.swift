6cbafabc5a093a8c7d77af23ef14c43962ffbf28
Leskeu <pro.theotherage@gmail.com>
2026-09-15 13:22:52 +0200

    Premier commit

---
+++ MealFestival/MealFestivalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealFestivalApp.swift
+//  MealFestival
+//
+//  Created by Leskeu  on 15/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct MealFestivalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
